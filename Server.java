56026db3d903cc2bcd7448898e1158a7f0566dca
Andrea Giugie <bisateo@gmail.com>
2016-10-20 09:47:22 +0200

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,11 +1,11 @@
 package server;
 
 /**
- * @author andrea.giugie
+ @author andrea.giugie
  */
 /**
- *
- * @author Giugie Andrea
+
+ @author Giugie Andrea
  */
 import java.io.*;
 import java.net.*;
@@ -14,51 +14,50 @@ import java.util.StringTokenizer;
 
 public class Server {
 
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 		ArrayList password = new ArrayList();
 		password.add("a");
 		password.add("b");
 		password.add("c");
-        ServerSocket echoServer = null;
-        String line;
-        BufferedReader is;
-        PrintStream os;
-        int port = 9999;
-        StringTokenizer parole;
-        Socket clientSocket = null;
-		ArrayList temp= password;
-		
-        try {
-            echoServer = new ServerSocket(port);    //Creo un socket alla porta 9999
-            System.out.println("Creato socket server in ascolto alla porta" + port);
-            clientSocket = echoServer.accept();          //Non appena viene individuato un socket client si creano i due stream
-            is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  //legge cosa invia il client
-            os = new PrintStream(clientSocket.getOutputStream());                           //invia al client
-            while (true) {
-				
-				
+		ServerSocket echoServer = null;
+		String line;
+		BufferedReader is;
+		PrintStream os;
+		int port = 9999;
+		StringTokenizer parole;
+		Socket clientSocket = null;
+		ArrayList temp = new ArrayList();
+		 temp = (ArrayList) password.clone();
 
-                line = is.readLine();
-                Thread.sleep(100);
-                if(temp.contains(line)){
+		try {
+			echoServer = new ServerSocket(port);    //Creo un socket alla porta 9999
+			System.out.println("Creato socket server in ascolto alla porta" + port);
+			clientSocket = echoServer.accept();          //Non appena viene individuato un socket client si creano i due stream
+			is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  //legge cosa invia il client
+			os = new PrintStream(clientSocket.getOutputStream());                           //invia al client
+			while (true) {
+
+				line = is.readLine();
+				Thread.sleep(100);
+				if (temp.contains(line)) {
 					temp.remove(temp.indexOf(line));
-				}
-				else{
-					temp=password;
-				}
-                
-                if (temp.size()==0) {  //condizione
-                    os.println("<QUIT>");
 
-                    echoServer.close();
-                    break;
-                }
-                os.println("ECHO " + line);
-            }
-        } catch (IOException e) {
-            System.out.println(e);
-        } catch (InterruptedException ie) {
-            System.out.println(ie);
-        }
-    }
+				} else {
+					temp = (ArrayList) password.clone();
+				}
+
+				if (temp.size() == 0) {  //condizione
+					os.println("<QUIT>");
+
+					echoServer.close();
+					break;
+				}
+				os.println("ECHO " + line);
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		} catch (InterruptedException ie) {
+			System.out.println(ie);
+		}
+	}
 }
