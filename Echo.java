de895786bd53c26d95c3e00a98117fdc73bce2f3
IridKozato <iridkozato97@gmail.com>
2016-10-21 21:09:51 +0200

    Add files via upload

---
+++ Echo.java
@@ -0,0 +1,48 @@
+
+import java.io.*;
+import java.net.*;
+import java.lang.Thread;
+
+public class Echo {
+
+    public static void main(String args[]) {
+
+        ServerSocket echoServer = null;
+        String line;
+        BufferedReader is;
+        PrintStream os;
+        String a;
+        int cont = 0;
+
+        Socket clientSocket = null;
+        try {
+            echoServer = new ServerSocket(9999); // creo il socket 
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        try {
+            clientSocket = echoServer.accept();// stabilisce la comunicazione
+            is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            os = new PrintStream(clientSocket.getOutputStream());
+            a = "chiudo"; // frase che una volta immessa dal client provoca la chiusura
+            while (true) {
+                line = is.readLine();
+                Thread.sleep(1000);
+                if (line.toLowerCase().equals(a)) {// controllo se la stringa che mi manda il client è uguale a quella di chiusura
+                    cont++;
+                    if (cont == 3) {
+                        System.out.println("Sto chiudendo la comunicazione");
+                        os.println("$chiudi$");
+                        echoServer.close();
+                    }
+
+                }
+                os.println("ECHO " + line);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (InterruptedException ie) {
+            System.out.println(ie);
+        }
+    }
+}
